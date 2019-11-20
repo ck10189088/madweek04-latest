e787704f4e2e4eb386d426287e89d5370ef767e0
Chong Chen Kang <s10189088@connect.np.edu.sg>
2019-11-20 15:47:23 +0800

    20 November

--- Telegramme/ContactController.swift
+++ Telegramme/ContactController.swift
@@ -30,6 +30,7 @@ class ContactController {
     
     func retrieveAllContact()->[Contact]{
         var contact:[NSManagedObject]=[]
+        var contactList:[Contact] = []
         
         let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
@@ -43,11 +44,41 @@ class ContactController {
                 let lastname = c.value(forKeyPath: "lastname") as? String
                 let mobileno = c.value(forKeyPath: "mobileno") as? String
                 print("\(firstname) \(lastname), \(mobileno!)")
+                contactList.append(Contact(firstname: "\(firstname)", lastname: "\(lastname)", mobileno: "\(mobileno)")) //difference between firstname and "\(firstname)"? why cannot just firstname
             }
             
         }catch let error as NSError{
             print("Could not fetch. \(error), \(error.userInfo)")
         }
-        return contact
+        return contactList
+    }
+    
+    func updateContact(mobileno:String,newContact: Contact){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CDContact")
+        fetchRequest.predicate = NSPredicate(format: "lastName = %@", "Doe-2")
+        
+        do{
+            let result = try managedContext.fetch(fetchRequest)
+            let obj = result[0] as! NSManagedObject
+            
+            obj.setValue("New John", forKeyPath: "firstName")
+            obj.setValue("Doe", forKeyPath: "lastName")
+            obj.setValue("New Mobile", forKeyPath: "mobile")
+            
+            do{
+                try managedContext.save()
+            } catch let error as NSError{
+            print("update error: \(error), \(error.userInfo)")
+            }
+        }
+        catch{
+            print("error")
+        }
+    }
+    
+    func deleteContact(mobileno:String){
+        
     }
 }
