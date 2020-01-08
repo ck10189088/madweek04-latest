7b010316aa8f5345b756b828dd2bfb75aa122fd4
Chong Chen Kang <s10189088@connect.np.edu.sg>
2020-01-08 15:44:03 +0800

    8 January

--- Telegramme/Mapviewcontroller.swift
+++ Telegramme/Mapviewcontroller.swift
@@ -11,14 +11,26 @@ import UIKit
 import CoreLocation
 import MapKit
 
+class customPin: NSObject,MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate:CLLocationCoordinate2D,title:String,subtitle:String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+}
+
 class Mapviewcontroller : UIViewController{
     
     @IBOutlet weak var map: MKMapView!
     let locationDelegate = LocationDelegate()
     var latestLocation: CLLocation? = nil
     let regionRadius: CLLocationDistance = 250
-    let annotation = MKPointAnnotation()
-
+    //let annotation = MKPointAnnotation()
     
     let locationManager : CLLocationManager = {
         $0.requestWhenInUseAuthorization()
@@ -35,16 +47,24 @@ class Mapviewcontroller : UIViewController{
         locationDelegate.locationCallback = { location in self.latestLocation = location
             let lat = String(format: "Lat: %3.8f", location.coordinate.latitude)
             let long = String(format: "Long: %3.8f", location.coordinate.longitude)
+            let lastlocation = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
             print("\(lat), \(long)")
             
             self.centerMapOnLocation(location: location)
+            
+            //self.annotation.coordinate = location.coordinate
+            //self.annotation.title = "Ngee Ann Polytechnic"
+            //self.annotation.subtitle = "School of ICT"
+            //self.map.addAnnotation(self.annotation)
+            
+            let annotation = customPin(coordinate: lastlocation, title: "Ngee Ann Polytechnic", subtitle: "School of ICT")
+            self.map.addAnnotation(annotation)
         }
     }
-        
+    
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
         map.setRegion(coordinateRegion, animated: true)
     }
-    
     
 }
